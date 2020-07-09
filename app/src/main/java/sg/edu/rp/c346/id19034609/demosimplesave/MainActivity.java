36a12029f1f2efb062a38878de03eb25803247d3
19034609 <Upsr173511>
2020-07-09 16:44:39 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19034609/demosimplesave/MainActivity.java
@@ -0,0 +1,34 @@
+package sg.edu.rp.c346.id19034609.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        prefEdit.putString("greetings", "Hello!");
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String msg = prefs.getString("greetings", "No greetings!");
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+    }
+}
